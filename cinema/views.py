538824c4b0d06aeeadcb1d77c34660a9fec82210
Roberto Camargo <jcamargoriv@gmail.com>
2022-06-28 16:57:05 -0500

    Se agrego plantilla HTML y seccion de cinema

--- cinema/views.py
+++ cinema/views.py
@@ -5,3 +5,5 @@ from . models import Movies, Genre, Directors
 def index(request):
     num_movies = Movies.objects.all().count()
     num_directors = Directors.objects.all().count()
+
+    return render(request, 'cinema/index.html', {})
