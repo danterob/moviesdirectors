569aebe083bff4b8116c7b366f2cf5f207fc878e
Roberto Camargo <jcamargoriv@gmail.com>
2022-06-29 17:13:34 -0500

    Agregada vista de detalles de pelicula

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('movie/<int:pk>/', views.movie_detail, name="movie_detail"),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Movies, Genre, Directors
 
 
 def index(request):
     directors = Directors.objects.all().order_by('fisrt_name')
+    directors
+    return render(request, 'cinema/index.html', {'directors': directors})
 
-    return render(request, 'cinema/index.html',
-                  {
-                      'directors': directors,
-                  }
-                  )
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movies, pk=pk)
+    return render(request, 'cinema/movie_detail.html', {'movie': movie})
